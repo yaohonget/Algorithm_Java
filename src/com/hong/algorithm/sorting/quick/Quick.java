04a8931e624221b8b9caa6e42a81af3f7ee68c80
Hong Yao <hyao@intershop.de>
2017-08-24 14:04:04 +0200

    # add second algorithm of quick sort

--- src/com/hong/algorithm/sorting/quick/Quick.java
+++ src/com/hong/algorithm/sorting/quick/Quick.java
@@ -4,18 +4,13 @@ import com.hong.algorithm.sorting.capi.Sort;
 
 public class Quick implements Sort {
 	
-	
-	
-	
-	
 	public static void main(String[] args) {
 		Quick quickSort = new Quick();
-		quickSort.sort();
-
+		//quickSort.sort();
+		quickSort.sort2();
 	}
 
-	public void init() {	
-		
+	public void init() {		
 	}
 
 	public void sort() {
@@ -36,27 +31,117 @@ public class Quick implements Sort {
 		System.out.println("execution time : " + executionTime);
 	}
 	
+	public void sort2() {
+		System.out.println("Quick sort input : ");
+		for(int i : input) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+		long startTime = System.nanoTime();
+		quickSort2(0, input.length - 1);
+		long endTime = System.nanoTime();
+		long executionTime = endTime - startTime;
+		System.out.println("Quick sort result : ");
+		for(int i : input) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+		System.out.println("execution time : " + executionTime);
+	}
+	
 	protected void quickSort(int startPos, int endPos) {	
 		
 		if(endPos < startPos) {
 			return;
-		}
-		
+		}		
 		int storePos = startPos;
-		int privot = input[endPos];
+		int pivot = input[endPos];
 		int searchEnd = endPos - 1;
 		for(int i = startPos; i <= searchEnd; i++) {			
-			if(input[i] < privot) {
+			if(input[i] < pivot) {
 				switchPos(i, storePos);
 				storePos++;				
 			}
-		}
-		
-		switchPos(endPos, storePos);
-		
+		}		
+		switchPos(endPos, storePos);		
 		quickSort(startPos, storePos - 1);
-		quickSort(storePos + 1, endPos);
-		
+		quickSort(storePos + 1, endPos);		
+	}
+	
+	protected void quickSort2(int pStartPos, int pEndPos) {
+		int startPos = pStartPos;
+		int endPos = pEndPos;
+		// for only one element
+		if(endPos <= startPos) {
+			return;
+		}
+		// for only two elements
+		else if((startPos + 1) == endPos) {
+			if( input[startPos] > input[endPos]) {
+				switchPos(startPos, endPos);
+			}			
+			return;
+		}
+		else {
+			// fix position of pivot, start and end
+			int pivotPos = startPos;	
+			int pivot = input[pivotPos];
+			startPos++;
+			do {
+				int foundEndPos = findEndPos(pivotPos, startPos, endPos);
+				int foundStartPos = findStartPos(pivotPos, startPos, foundEndPos);
+				if(foundStartPos < foundEndPos) {
+					switchPos(foundStartPos, foundEndPos);
+				}
+				else {
+					if(pivot > input[foundEndPos]) {
+						switchPos(pivotPos, foundEndPos);
+						pivotPos = foundEndPos;
+					}
+					break;
+				}
+			}
+			while(true);
+			quickSort2(pStartPos, pivotPos - 1);
+			quickSort2(pivotPos + 1, pEndPos);
+			return;
+		}
+	}
+	
+	protected int findEndPos(int pivotPos, int startPos, int endPos) {
+		int foundEndPos = endPos;
+		while(true) {
+			if(endPos <= startPos) {
+				foundEndPos = startPos; 
+				break;
+			}
+			if(input[endPos] > input[pivotPos]) {
+				endPos--;				
+			}
+			else {
+				foundEndPos = endPos;
+				break;
+			}
+		}
+		return foundEndPos;
+	}
+	
+	protected int findStartPos(int pivotPos, int startPos, int endPos) {
+		int foundStartPos = startPos;
+		while(true) {
+			if(foundStartPos >= endPos) {
+				foundStartPos = endPos;
+				break;
+			}
+			if(input[startPos] <= input[pivotPos]) {
+				startPos++;				
+			}
+			else {
+				foundStartPos =startPos;				
+				break;
+			}
+		}
+		return foundStartPos;
 	}
 	
 	protected void switchPos(int posA, int posB) {
@@ -64,5 +149,4 @@ public class Quick implements Sort {
 		input[posA] = input[posB];
 		input[posB] = tmp;
 	}
-
 }
